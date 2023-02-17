e79672ac5b73c951519edb41aae41bfb324816a2
leviatan1324 <alonsoeduardo.1324@gmail.com>
2023-02-17 11:58:58 -0600

    version inicial del proyecto project-jpa

---
+++ src/main/java/com/edwar/project_jpa/App.java
@@ -0,0 +1,13 @@
+package com.edwar.project_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
